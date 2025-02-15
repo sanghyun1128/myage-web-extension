26d592fe3dc038fdabfef83d97527758b205cc7e
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-15 18:29:57 +0900

    chore: add webpack configuration for building popup bundle

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // 배포 시 'production'으로 변경하세요.
+  entry: './src/popup/popup.js',
+  output: {
+    filename: 'popup.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
