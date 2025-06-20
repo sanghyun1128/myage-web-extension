f95d32eca8b734735f44f0e6b31fd788a4befe80
sanghyun1128 <sanghyun1128@gmail.com>
2025-06-20 14:42:17 +0900

    fix: ensure default values are set only on extension installation

--- src/background.js
+++ src/background.js
@@ -1,6 +1,10 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ birth: '' });
-  chrome.storage.sync.set({ exam: 0 });
-  chrome.storage.sync.set({ army: '' });
-  chrome.storage.sync.set({ fast: false });
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    chrome.storage.sync.set({
+      birth: '',
+      exam: '0',
+      army: '',
+      fast: false,
+    });
+  }
 });
